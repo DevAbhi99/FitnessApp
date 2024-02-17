5e43d1bd38d439d76782872ae0fd2963ae8102c9
DevAbhi99 <abhigyankhaund659@gmail.com>
2024-02-17 10:46:36 +0530

    fitnessapp

---
+++ src/main/java/com/mycompany/fitnessapp/FitnessApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fitnessapp;
+
+/**
+ *
+ * @author TIRTHANKAR KHAUND
+ */
+public class FitnessApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
